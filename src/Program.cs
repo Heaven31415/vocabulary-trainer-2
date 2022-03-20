956ebb9515965a720357a3cc2728aadc5f41358f
heaven31415 <heaven31415@gmail.com>
2022-03-21 00:11:20 +0100

    Implement creating nouns from CSV files.

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,49 @@
-﻿namespace VocabularyTrainer2
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace VocabularyTrainer2
 {
     internal class Program
     {
         static void Main()
         {
             Console.Title = "Vocabulary Trainer 2";
-            Console.WriteLine("Hello World!");
+
+            var nouns = ReadNounsFromCSVFile("data/Nouns.csv");
+
+            foreach (var noun in nouns)
+            {
+                Utility.WriteLine($"{noun.EnglishDescription}");
+            }
+        }
+
+        static List<Noun> ReadNounsFromCSVFile(string path)
+        {
+            var nouns = new List<Noun>();
+
+            using var reader = new StreamReader(path);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            csv.Read();
+            csv.ReadHeader();
+
+            while (csv.Read())
+            {
+                string englishDescription = csv.GetField(0);
+                string? germanSingularForm = csv.GetField(1);
+                string? germanPluralForm = csv.GetField(2);
+
+                if (string.IsNullOrEmpty(germanSingularForm))
+                    germanSingularForm = null;
+
+                if (string.IsNullOrEmpty(germanPluralForm))
+                    germanPluralForm = null;
+
+                var noun = new Noun(englishDescription, germanSingularForm, germanPluralForm);
+                nouns.Add(noun);
+            }
+
+            return nouns;
         }
     }
 }
