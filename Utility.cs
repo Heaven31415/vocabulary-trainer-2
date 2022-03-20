04c1c7f2eb86b50a1da98a7ad8de9d70a94cff1a
heaven31415 <heaven31415@gmail.com>
2022-03-20 23:38:58 +0100

    Add class with utility methods.

---
+++ Utility.cs
@@ -0,0 +1,29 @@
+﻿namespace VocabularyTrainer2
+{
+    internal class Utility
+    {
+        public static void Write(string? value = null, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ResetColor();
+        }
+
+        public static void WriteLine(string? value = null, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+
+        public static string ReadLine()
+        {
+            var line = Console.ReadLine();
+
+            if (line == null)
+                throw new Exception("Unable to read line from standard input.");
+
+            return line;
+        }
+    }
+}
