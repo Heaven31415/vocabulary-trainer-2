101a999722d9c9c8458d9dd7b5dafb571b69b9e5
heaven31415 <heaven31415@gmail.com>
2022-03-21 23:12:33 +0100

    Add initial Adjective implementation.

---
+++ src/Adjective.cs
@@ -0,0 +1,26 @@
+﻿namespace VocabularyTrainer2
+{
+    internal class Adjective
+    {
+        public string EnglishDescription { get; }
+        public string PositiveDegree { get; }
+        public string? ComparativeDegree { get; }
+        public string? SuperlativeDegree { get; }
+
+        public Adjective(string englishDescription, string positiveDegree)
+        {
+            EnglishDescription = englishDescription;
+            PositiveDegree = positiveDegree;
+            ComparativeDegree = null;
+            SuperlativeDegree = null;
+        }
+
+        public Adjective(string englishDescription, string positiveDegree, string comparativeDegree, string superlativeDegree)
+        {
+            EnglishDescription = englishDescription;
+            PositiveDegree = positiveDegree;
+            ComparativeDegree = comparativeDegree;
+            SuperlativeDegree = superlativeDegree;
+        }
+    }
+}
