c6ef3fc976d33ae7976e84da3fe722c2496954e7
heaven31415 <heaven31415@gmail.com>
2022-03-21 01:03:22 +0100

    Implement simple Flashcard test.

--- src/Program.cs
+++ src/Program.cs
@@ -10,11 +10,35 @@ namespace VocabularyTrainer2
             Console.Title = "Vocabulary Trainer 2";
 
             var nouns = ReadNounsFromCSVFile("data/Nouns.csv");
+            var flashcards = new List<Flashcard>();
 
             foreach (var noun in nouns)
             {
-                Utility.WriteLine($"{noun.EnglishDescription}");
+                if (noun.GermanSingularForm != null)
+                {
+                    string question = $"{noun.EnglishDescription} (singular)";
+                    flashcards.Add(new Flashcard(question, $"{noun.Gender.ToArticle()} {noun.GermanSingularForm}"));
+                }
+
+                if (noun.GermanPluralForm != null)
+                {
+                    string question = $"{noun.EnglishDescription} (plural)";
+                    flashcards.Add(new Flashcard(question, $"die {noun.GermanPluralForm}"));
+                }
             }
+
+            var flashcard = flashcards[0];
+
+            Utility.WriteLine($"Translate to german: '{flashcard.AskQuestion()}'");
+            Utility.Write("Answer: ");
+            var answer = Utility.ReadLine();
+
+            var (isCorrect, correctAnswer) = flashcard.GiveAnswer(answer);
+
+            if (isCorrect)
+                Utility.WriteLine("You are correct!", ConsoleColor.Green);
+            else
+                Utility.WriteLine($"This is not correct. The correct answer is: '{correctAnswer}'", ConsoleColor.Red);
         }
 
         static List<Noun> ReadNounsFromCSVFile(string path)
