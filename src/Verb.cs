868e361a0f36207c2a64de5d6673fd89757a3195
heaven31415 <heaven31415@gmail.com>
2022-03-26 01:51:07 +0100

    Add Verb and VerbExtractor classes.

---
+++ src/Verb.cs
@@ -0,0 +1,30 @@
+﻿namespace VocabularyTrainer2
+{
+    enum PersonalPronoun
+    {
+        FirstSingular,
+        SecondSingular,
+        ThirdSingular,
+        FirstPlural,
+        SecondPlural,
+        ThirdPlural
+    }
+
+    internal class Verb
+    {
+        public int Id { get; set; }
+        public string EnglishDescription { get; set; }
+        public Dictionary<PersonalPronoun, string> Present { get; set; }
+        public Dictionary<PersonalPronoun, string> SimplePast { get; set; }
+        public Dictionary<PersonalPronoun, string> Perfekt { get; set; }
+
+        public Verb(int id, string englishDescription)
+        {
+            Id = id;
+            EnglishDescription = englishDescription;
+            Present = new Dictionary<PersonalPronoun, string>();
+            SimplePast = new Dictionary<PersonalPronoun, string>();
+            Perfekt = new Dictionary<PersonalPronoun, string>();
+        }
+    }
+}
