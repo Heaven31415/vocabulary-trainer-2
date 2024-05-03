3399a9ad07957e651e918547983d72b1a51a7f23
Heaven31415 <heaven31415@gmail.com>
2024-05-03 12:30:46 +0200

    Change IsCapitalized extension method

--- Source/Common/Extensions.cs
+++ Source/Common/Extensions.cs
@@ -7,7 +7,7 @@ namespace VocabularyTrainer2.Source.Common
     {
         public static int Digit(this int i, int n) => i / (int)Math.Pow(10, n - 1) % 10;
         public static string Capitalize(this string str) => string.Concat(str[..1].ToUpper(), str.AsSpan(1));
-        public static bool IsCapitalized(this string str) => str == str.ToLower().Capitalize();
+        public static bool IsCapitalized(this string str) => str[0] == str.Capitalize()[0];
         public static bool IsLower(this string str) => str == str.ToLower();
         public static string Hash(this string str)
         {
